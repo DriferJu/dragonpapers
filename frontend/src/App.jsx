536ae08f323bbb0ede0654a777d1a48daf9b6cc4
JULIEN BONET <julienbonetpro@gmail.com>
2024-04-03 15:03:24 +0200

    created header footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
-import "./App.css";
+import Header from "./components/Navigation/Header/Header";
+import Footer from "./components/Navigation/Footer/Footer";
 
 function App() {
   return (
     <main>
+      <Header />
       <Outlet />
+      <Footer />
     </main>
   );
 }
