c7db9520a883b8baae0c40fb316adf9b0115296e
JULIEN BONET <julienbonetpro@gmail.com>
2024-04-03 16:25:10 +0200

    Menu responsive burger

--- frontend/src/components/Navigation/Navbar/Navbar.jsx
+++ frontend/src/components/Navigation/Navbar/Navbar.jsx
@@ -1,19 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
 import "./navbar.css";
+import DragonLogo from "../../../assets/dnd_ico/dragon_red.png";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
   return (
     <section className="top-nav">
-      <div>Dragon Papers</div>
+      <div
+        role="button"
+        className="NavLogo"
+        onClick={() => handleNavigation("/")}
+        tabIndex={0}
+      >
+        <img src={DragonLogo} alt="dragonLogo" className="NavDragonLogo" />
+        <h1 className="NavTitle">DRAGON PAPERS</h1>
+      </div>
       <input id="menu-toggle" type="checkbox" />
       <label className="menu-button-container" htmlFor="menu-toggle">
         <div className="menu-button" />
       </label>
       <ul className="menu">
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-        <li>Four</li>
-        <li>Five</li>
+        <li onClick={() => handleNavigation("/charactersheet")}>
+          CHARACTER SHEET
+        </li>
       </ul>
     </section>
   );
