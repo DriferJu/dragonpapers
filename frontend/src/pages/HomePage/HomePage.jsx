6629687b1b7ee0eb1641b05ba70bb52f894f245d
JULIEN BONET <julienbonetpro@gmail.com>
2024-04-04 16:41:49 +0200

    created register bloc in homepage + toggle Login /register

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import "./homepage.css";
 import Dragonred from "../../assets/dnd_ico/dragon_red.png";
 import DnD from "../../assets/dnd_ico/D&D_red2.png";
 
 function HomePage() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  function handleRegister() {
+    setIsLogin(false);
+  }
+
+  function handleLogin() {
+    setIsLogin(true);
+  }
+
   return (
     <section className="homepage">
       <section className="HP-col1">
@@ -13,31 +24,110 @@ function HomePage() {
             <h1 className="HP_Main_title">DRAGON PAPERS</h1>
             <div className="red-barre"> </div>
           </div>
-
-          <div className="scrollpaper_container">
-            <div className="scrollpaper_contents">
-              <div className="HP_input_container">
-                <h2>Login</h2>
-                <input type="text" name="text" placeholder="enter your login" />
-              </div>
-              <div className="HP_input_container">
-                <h2>password</h2>
-                <input
-                  type="password"
-                  name="text"
-                  placeholder="enter your password"
-                />
-              </div>
-              <button type="submit" className="HP_button">
-                VALIDATE
-              </button>
-              <div className="log_options_line">
-                <p className="HP_log_options">log-in</p>
-                <p>|</p>
-                <p className="HP_log_options">register</p>
+          {isLogin && (
+            <div className="scrollpaper_container">
+              <div className="scrollpaper_contents">
+                <div className="HP_input_container">
+                  <h2>Login</h2>
+                  <input
+                    type="text"
+                    name="text"
+                    placeholder="enter your login"
+                  />
+                </div>
+                <div className="HP_input_container">
+                  <h2>password</h2>
+                  <input
+                    type="password"
+                    name="text"
+                    placeholder="enter your password"
+                  />
+                </div>
+                <button type="submit" className="HP_button">
+                  VALIDATE
+                </button>
+                <div className="log_options_line">
+                  <p className="HP_log_options_active">log-in</p>
+                  <p>|</p>
+                  <p
+                    className="HP_log_options"
+                    onClick={() => {
+                      handleRegister();
+                    }}
+                  >
+                    register
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          )}
+          {!isLogin && (
+            <div className="scrollpaper_container_register">
+              <div className="scrollpaper_contents_register">
+                <div className="HP_input_container">
+                  <h2>choose Pseudo</h2>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    minLength="4"
+                    maxLength="8"
+                    size="20"
+                  />
+                </div>
+                <div className="HP_input_container">
+                  <h2>enter Email</h2>
+
+                  <input
+                    type="email"
+                    id="email"
+                    pattern=".+@example\.com"
+                    size="20"
+                    required
+                  />
+                </div>
+                <div className="HP_input_container">
+                  <h2>choose Password</h2>
+                  <input
+                    type="password"
+                    id="pass"
+                    name="password"
+                    minLength="8"
+                    size="20"
+                    required
+                  />
+                </div>
+                <div className="HP_input_container">
+                  <h2>confirm Password</h2>
+                  <input
+                    type="password"
+                    id="pass"
+                    name="password"
+                    minLength="8"
+                    size="20"
+                    required
+                  />
+                </div>
+                <button type="submit" className="HP_button" size="15">
+                  SUBMIT
+                </button>
+                <div className="log_options_line">
+                  <p
+                    className="HP_log_options"
+                    onClick={() => {
+                      handleLogin();
+                    }}
+                  >
+                    log-in
+                  </p>
+                  <p>|</p>
+                  <p className="HP_log_options_active">register</p>
+                </div>
+              </div>
+            </div>
+          )}
+
           <div className="HP_introtext_container">
             <p className="HP_intro_text">
               Create your charactersheets and find ressources to roleplay
